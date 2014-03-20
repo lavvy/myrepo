18679040ae878e1e94c440ec74e85dc42c8d1e98
lavvy <lavashonline@gmail.com>
2014-03-20 09:26:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -10,12 +10,12 @@ defined('PHP') or exit('NO DICE!');
 
 class Videoplus_Service_apimethods_getmymails extends Php_Service 
 
-  {
+                                                              {
 
 
 
 
-  public function process()
+              public function process()
 	{
 	return	Php::getService('mail.api')->get();
 	}
